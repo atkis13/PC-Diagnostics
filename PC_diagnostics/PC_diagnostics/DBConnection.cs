7f528e12820f687d3076137a41b118a4b63859bd
atkis13 <kis_attila_1349@yahoo.com>
2018-02-28 19:00:53 +0200

    commit 2
    added method stubs

--- PC_diagnostics/PC_diagnostics/DBConnection.cs
+++ PC_diagnostics/PC_diagnostics/DBConnection.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 
+
 namespace PC_diagnostics
 {
     class DBConnection
---
+++ PC_diagnostics/PC_diagnostics/Form_Methods.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace PC_diagnostics
+{
+    class Form_Methods
+    {
+        static DBConnection conn;
+
+        public static void AddConfigtoDB(string id, string processor, string mobo, string ram, string hdd, string video_card, string psu, string os)
+        {
+
+        } 
+
+        public static void AddPCtoDB(string id, string owner)
+        {
+
+        }
+
+        public static void GetConfigFromDB(Label id, Label processor, Label mobo, Label ram, Label hdd, Label video_card, Label psu, Label os)
+        {
+
+        }
+
+        public static void GetPCOwnerFromDB(Label owner)
+        {
+
+        }
+
+        public static void CreateNewPDFLog() // use to update the pdf as well
+        {
+
+        }
+
+        public static void ReadPDFLog()
+        {
+
+        }
+
+        public static void FIllCombo(ComboBox cb)
+        {
+
+        }
+    }
+}
